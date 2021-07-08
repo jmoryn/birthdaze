2446ac75ad1a54994fd9ea308e802569177bb197
Jesse Moryn <macuser@Macs-MacBook-Pro.local>
2021-07-08 17:40:12 -0500

    first commit, create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1>Welcome to Birthdaze</h1>
+    </div>
+  );
+}
+
+export default App;
